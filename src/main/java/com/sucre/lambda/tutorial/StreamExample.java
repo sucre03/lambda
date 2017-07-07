6236411abd99876200c62a022685b9aa7e45adb4
sucre03 <sucre03@live.cn>
2017-07-07 10:34:23 +0800

    partitioningBy 和 mapping

--- src/main/java/com/sucre/lambda/tutorial/StreamExample.java
+++ src/main/java/com/sucre/lambda/tutorial/StreamExample.java
@@ -1,12 +1,12 @@
 package com.sucre.lambda.tutorial;
 
-import com.google.common.collect.Lists;
+import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
 
 import java.math.BigDecimal;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
 
 /**
  * Describe:stream的调试
@@ -39,7 +39,7 @@ public class StreamExample {
         List<String> fruits = Arrays.asList("apple", "apple", "banana",
                         "apple", "orange", "banana", "papaya");
         Map<String,Long> result =
-                fruits.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                fruits.stream().collect(groupingBy(Function.identity(), counting()));
         System.out.println(result);
         //LinkedHashMap的顺序默认为插入顺序
         Map<String,Long> finalMap = Maps.newLinkedHashMap();
@@ -59,15 +59,23 @@ public class StreamExample {
                 Item.getInstance("apple", 10, new BigDecimal("9.99"))
         );
         Map<String,Long> counting = items.stream()
-                .collect(Collectors.groupingBy(Item::getName,Collectors.counting()));
+                .collect(groupingBy(Item::getName,counting()));
         System.out.println(counting);
         Map<String,Integer> sum = items.stream()
-                .collect(Collectors.groupingBy(Item::getName,Collectors.summingInt(Item::getQty)));
+                .collect(groupingBy(Item::getName,summingInt(Item::getQty)));
         System.out.println("1======:"+sum);
-        Map<BigDecimal,List<Item>> groupByPriceMap = items.stream().collect(Collectors.groupingBy(Item::getPrice));
+        Map<BigDecimal,List<Item>> groupByPriceMap = items.stream().collect(groupingBy(Item::getPrice));
         System.out.println("2======:"+groupByPriceMap);
-        Map<BigDecimal,Set<String>> res = items.stream().collect(Collectors.groupingBy(Item::getPrice,Collectors.mapping(Item::getName,Collectors.toSet())));
+        Map<BigDecimal,Set<String>> res = items.stream().collect(groupingBy(Item::getPrice,mapping(Item::getName,toSet())));
         System.out.println("3======:"+res);
+        Map<BigDecimal,String> res1 = items.stream().collect(groupingBy(Item::getPrice,mapping(Item::getName, joining(";"))));
+        System.out.println(res1+"===");
+        Optional<String> max = items.stream().max(Comparator.comparing(Item::getPrice)).map(Item::getName);
+        System.out.println("max price fruit is:"+max.orElse(""));
+        //在List上使用partitioningBy，满足条件的生成一个List，不满足条件的生成另一个List
+        Map<Boolean,List<Item>> pm = items.stream().collect(partitioningBy(t->t.getQty()==10));
+        pm.entrySet().stream().forEach(t->
+            System.out.println(t.getKey()+":"+t.getValue()));
     }
 
 
